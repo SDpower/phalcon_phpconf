e8852a20050381637ed0f6453f7b0c6cf224a468
SDpower <info@sd.idv.tw>
2014-10-14 10:29:04 +0800

    Fix service and view dibble level.

--- config/services.php
+++ config/services.php
@@ -15,22 +15,25 @@ if (APP_ENV == "development") {
 /**
  * Sets the view component
  */
-$di['view'] = function () use ($config) {
+$di->set('view', function () use ($config) {
     $view = new View();
     $view->setViewsDir($config->application->viewsDir);
-
+    $view->disableLevel(array(
+        Phalcon\Mvc\View::LEVEL_LAYOUT => true,
+        Phalcon\Mvc\View::LEVEL_MAIN_LAYOUT => true
+    ));
     return $view;
-};
+},true);
 
 /**
  * The URL component is used to generate all kind of urls in the application
  */
-$di['url'] = function () use ($config) {
+$di->set('url', function () use ($config) {
     $url = new UrlResolver();
     $url->setBaseUri($config->application->baseUri);
 
     return $url;
-};
+},true);
 
 /**
  * Database connection is created based in the parameters defined in the configuration file
